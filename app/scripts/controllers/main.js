68bab358e20f21e4c58df37f08a506673e063b1f
Dennis Haegler <dennis.haegler@gmail.com>
2015-11-15 15:16:34 +0100

    fasdfasdf

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,6 +7,8 @@ angular.module('ascii2BrainApp')
       'AngularJS',
       'Karma'
     ];
+
+    this.data = data_geek;
   });
 
 angular.module('ascii2BrainApp')
@@ -20,9 +22,34 @@ angular.module('ascii2BrainApp')
 
 angular.module('ascii2BrainApp')
   .controller('GeekCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+    this.data = {
+      "!": 33,
+      '"': 34,
+      "#": 35,
+      "$": 36,
+      "%": 37,
+      "&": 38,
+      "'": 39,
+      "(": 40,
+      ")": 41,
+      "*": 42,
+      "+": 43,
+      ",": 44,
+      "-": 45,
+      ".": 46,
+      "/": 47,
+      "0": 48,
+      "1": 49,
+      "2": 50,
+      "3": 51,
+      "4": 52,
+      "5": 53,
+      "6": 54,
+      "7": 55,
+      "8": 56,
+      "9": 57,
+      ":": 58,
+      ";": 59
+    };
+
   });
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 app.use('/scripts',  express.static(__dirname + '/app/scripts'));
 app.use('/views',  express.static(__dirname + '/app/views'));
 app.use('/styles',  express.static(__dirname + '/app/styles'));
+app.use('/images',  express.static(__dirname + '/app/images'));
 
 app.get('/', function(req, res) {
   res.sendfile('app/index.html');
